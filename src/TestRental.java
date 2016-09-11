0bc2b2cd7c479ce1ced71715bf833b143b5a972d
WendaDeng <dwd1044898101@gmail.com>
2016-09-11 19:26:51 +0800

    修改第一次重构生成的accountFor函数中的局部变量，使其更加直观易懂

--- src/TestRental.java
+++ src/TestRental.java
@@ -9,11 +9,11 @@ public class TestRental {
 
 	public static void main(String[] args) {
 		// Movies
-		Movie movie1 = new Movie("The Little Prince", 2);
-		Movie movie2 = new Movie("Avatar", 1);
-		Movie movie3 = new Movie("Bravery Heart", 0);
+		Movie movie1 = new Movie("The Little Prince", Movie.CHILDRENS);	
+		Movie movie2 = new Movie("Avatar", Movie.NEW_RELEASE);				
+		Movie movie3 = new Movie("Bravery Heart", Movie.REGULAR);
 		// Rental records
-		Rental rental1 = new Rental(movie1, 3);
+		Rental rental1 = new Rental(movie1, 4);
 		Rental rental2 = new Rental(movie2, 3);
 		Rental rental3 = new Rental(movie3, 3);
 		
